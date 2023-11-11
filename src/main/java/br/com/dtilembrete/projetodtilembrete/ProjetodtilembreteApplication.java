6b03311b77a1d462646dd95f63e587a8adf1e5be
Gabriel Azevedo <darkworse@gmail.com>
2023-11-11 14:17:15 -0300

    + Big Init

---
+++ src/main/java/br/com/dtilembrete/projetodtilembrete/ProjetodtilembreteApplication.java
@@ -0,0 +1,13 @@
+package br.com.dtilembrete.projetodtilembrete;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetodtilembreteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetodtilembreteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/dtilembrete/projetodtilembrete/ProjetodtilembreteApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.dtilembrete.projetodtilembrete;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetodtilembreteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
